97cf4360db6eac8705f32d44bb03e87e7972a129
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2024-01-02 16:46:41 +0100

    iniciei o proecto

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+function antionMenu() {
+    const btn = document.getElementById('toggle');
+    const menu = document.getElementById('navbar-mobile');
+    btn.classList.toggle('open');
+    menu.classList.toggle('openMenu');
+}
